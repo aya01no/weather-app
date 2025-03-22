d43f8caec16a8fb5cb25493a9a758f1f1579b3a7
aya01no <kurimaru0105@icloud.com>
2025-03-22 14:09:44 +0900

    add css

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
+import './App.css';
 
 const Weather = () => {
   const [city, setCity] = useState("");
@@ -9,29 +10,44 @@ const Weather = () => {
   const apiKey = process.env.NEXT_PUBLIC_OPENWEATHERMAP_API_KEY;
 
 const fetchWeather = async ()  => {
+  try {
   const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ja`);
+  
+  if (!res.ok) {
+    throw new Error(`HTTPエラー:${res.status}`);
+  }
+
   const date = await res.json();
   setWeather(date);
+} catch (error) {
+  console.error("天気情報の取得に失敗", error.message);
+}
 };
 
 return(
-  <div>
+  <div className="container">
+  <div className="form-area">
     <input
     type="text"
     value={city}
     onChange={(e) => setCity(e.target.value)}
     placeholder="都市名を入力"
+    className="input"
     />
-    <button onClick={fetchWeather}>天気を確認</button>
+    <button onClick={fetchWeather} className="btn">
+      天気を確認
+      </button>
+
     {
       weather && (
-        <div>
-          <p>{weather.name}</p>
-          <p>{weather.weather?.[0]?.description}</p>
-          <p>{weather.main?.temp}℃</p>
+        <div className="result-area">
+          <p className="city-name">{weather.name}</p>
+          <p className="description">{weather.weather[0].description}</p>
+          <p className="temp">{weather.main.temp}℃</p>
         </div>
       )
     }
+  </div>
   </div>
 );
 };
